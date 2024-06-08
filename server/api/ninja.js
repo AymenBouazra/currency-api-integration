016718f47b2f095667ae6b6ded1caa0eab706bbd
Aymen <aymenbouazra994@gmail.com>
2024-06-08 06:06:47 +0100

    hotfix

--- server/api/ninja.js
+++ server/api/ninja.js
@@ -5,7 +5,8 @@ export default defineEventHandler(async (event) => {
     // const { age } = await readBody(event)
 
     //api call with private key 
-    const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=cur_live_u99tVbFKFFotOMKIAiyBCDa0a54WjEW0DeCOgRTS')
+    const { currencyKey } = useRuntimeConfig()
+    const { data } = await $fetch(`https://api.currencyapi.com/v3/latest?apikey=${currencyKey}`)
 
     return data
 })
